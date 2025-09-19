97bff1267ce221d3a84bd6ad42f74d5dd8a6eac4
SzabolcsBodi756 <90689769+SzabolcsBodi756@users.noreply.github.com>
2025-09-19 17:26:05 +0200

    Minden ready

--- OOPAdatbázis/Services/Cars.cs
+++ OOPAdatbázis/Services/Cars.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
 using OOPAdatbázis;
 using OOPAdatbázis.Services;
 using System.Collections.Generic;
@@ -10,12 +11,56 @@ namespace OOPAdatbázis.Services
     {
         public object AddNewItem(object newRecord)
         {
-            throw new NotImplementedException();
+            Connect conn = new Connect("libary");
+
+            conn.Connection.Open();
+
+            string sql = "INSERT INTO cars(brand, type, mDate) VALUES (@brand, @type, @mDate)";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            var car = newRecord.GetType().GetProperties();
+
+            cmd.Parameters.AddWithValue("@brand", car[0].GetValue(newRecord));
+            cmd.Parameters.AddWithValue("@type", car[1].GetValue(newRecord));
+            cmd.Parameters.AddWithValue("@mDate", car[2].GetValue(newRecord));
+
+            cmd.ExecuteNonQuery();
+
+            conn.Connection.Close();
+
+            var result = new
+            {
+                message = "Sikeres felvétel",
+                result = newRecord
+            };
+
+            return result;
         }
 
         public object DeleteItem(int id)
         {
-            throw new NotImplementedException();
+            Connect conn = new Connect("libary");
+
+            conn.Connection.Open();
+
+            string sql = "DELETE FROM `cars` WHERE id = @id";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Connection.Close();
+
+            var result = new
+            {
+                message = "Sikeres törlés.",
+
+            };
+
+            return result;
         }
 
         public object GetById(int id)
@@ -49,7 +94,32 @@ namespace OOPAdatbázis.Services
 
         public object UpdateItem(object modifiedItem)
         {
-            throw new NotImplementedException();
+            Connect conn = new Connect("libary");
+
+            conn.Connection.Open();
+
+            string sql = "UPDATE cars SET brand = @brand, type = @type, mDate = @mDate  WHERE Id = @id";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            var car = modifiedItem.GetType().GetProperties();
+
+            cmd.Parameters.AddWithValue("@id", car[0].GetValue(modifiedItem));
+            cmd.Parameters.AddWithValue("@brand", car[1].GetValue(modifiedItem));
+            cmd.Parameters.AddWithValue("@type", car[2].GetValue(modifiedItem));
+            cmd.Parameters.AddWithValue("@mDate", car[3].GetValue(modifiedItem));
+
+            cmd.ExecuteNonQuery();
+
+            conn.Connection.Close();
+
+            var result = new
+            {
+                message = "Sikeres felvétel",
+                result = modifiedItem
+            };
+
+            return result;
         }
 
         public List<object> GetAllData(string dbName)
